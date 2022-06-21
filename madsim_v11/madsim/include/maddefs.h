ecbc3a724df083cc61e671964bde348e80b28ea2
Weixi Zhu <wxzhu@rice.edu>
2022-06-21 12:21:42 -0500

    change to freebsd makefile

--- madsim_v11/madsim/include/maddefs.h
+++ madsim_v11/madsim/include/maddefs.h
@@ -517,6 +517,90 @@ static inline int add_uevent_var(struct kobj_uevent_env *env, const char *format
     return 0;
 }
 
+enum probe_type {
+    PROBE_DEFAULT_STRATEGY,
+    PROBE_PREFER_ASYNCHRONOUS,
+    PROBE_FORCE_SYNCHRONOUS,
+};
+
+#define _DEVICE_H_
+
+struct device {
+    struct kobject kobj;
+    struct device       *parent;
+
+    struct device_private   *p;
+
+    const char      *init_name; /* initial name of the device */
+    const struct device_type *type;
+
+    struct bus_type *bus;       /* type of bus device is on */
+    struct device_driver *driver;   /* which driver has allocated this
+                       device */
+    void        *platform_data; /* Platform specific data, device
+                       core doesn't touch it */
+    void        *driver_data;   /* Driver data, set and get with
+                       dev_set_drvdata/dev_get_drvdata */
+    struct mutex        mutex;  /* mutex to synchronize calls to
+                     * its driver.
+                     */
+
+    struct dev_links_info   links;
+    struct dev_pm_info  power;
+    struct dev_pm_domain    *pm_domain;
+
+    struct irq_domain   *msi_domain;
+    struct dev_pin_info *pins;
+    struct list_head    msi_list;
+
+    const struct dma_map_ops *dma_ops;
+    u64     *dma_mask;  /* dma mask (if dma'able device) */
+    u64     coherent_dma_mask;/* Like dma_mask, but for
+                         alloc_coherent mappings as
+                         not all hardware supports
+                         64 bit addresses for consistent
+                         allocations such descriptors. */
+    u64     bus_dma_limit;  /* upstream dma constraint */
+    unsigned long   dma_pfn_offset;
+
+    struct device_dma_parameters *dma_parms;
+
+    struct list_head    dma_pools;  /* dma pools (if dma'ble) */
+
+    struct dma_coherent_mem *dma_mem; /* internal for coherent mem
+                         override */
+    struct cma *cma_area;       /* contiguous memory area for dma
+                       allocations */
+    /* arch specific additions */
+    struct dev_archdata archdata;
+
+    struct device_node  *of_node; /* associated device tree node */
+    struct fwnode_handle    *fwnode; /* firmware device node */
+
+    int     numa_node;  /* NUMA node this device is close to */
+    dev_t           devt;   /* dev_t, creates the sysfs "dev" */
+    u32         id; /* device instance */
+
+    spinlock_t      devres_lock;
+    struct list_head    devres_head;
+
+    struct class        *class;
+    const struct attribute_group **groups;  /* optional groups */
+
+    void    (*release)(struct device *dev);
+    struct iommu_group  *iommu_group;
+    struct dev_iommu    *iommu;
+
+    bool            offline_disabled:1;
+    bool            offline:1;
+    bool            of_node_reused:1;
+    bool            state_synced:1;
+#if defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_DEVICE) || \
+    defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU) || \
+    defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU_ALL)
+    bool            dma_coherent:1;
+#endif
+};
 
 struct linux_5_7_19_device_driver {
     const char      *name;
