f11606d6ba52424dc3f61dd05d9e11c1d736a7fc
Weixi Zhu <wxzhu@rice.edu>
2022-06-30 13:55:43 -0500

    debug

--- mad_cdev/vm_cdev.c
+++ mad_cdev/vm_cdev.c
@@ -16,7 +16,7 @@ static inline uint64_t *get_pte(vm_page_t pgroot, vm_offset_t va, int lvl) {
     uint64_t *pde;
     if (lvl == 0) {
         pde = (uint64_t *) PHYS_TO_DMAP(VM_PAGE_TO_PHYS(&pgroot[get_lvl_index(va, 0) >> 9]));
-        printf("[get_pte] %lx %d, pg index %d, index: %d, pde: %p\n", va, lvl,
+        printf("[get_pte] %lx %d, pg index %lu, index: %lu, pde: %p\n", va, lvl,
             get_lvl_index(va, 0) >> 9, get_lvl_index(va, 0) & LVL_MASK, pde);
         return &pde[get_lvl_index(va, 0) & LVL_MASK];
     } else {
@@ -27,7 +27,7 @@ static inline uint64_t *get_pte(vm_page_t pgroot, vm_offset_t va, int lvl) {
             // flush device cache.
         }
         pde = (uint64_t *) PHYS_TO_DMAP(*pde);
-        printf("[get_pte] %lx %d, index: %d, pde: %p\n", va, lvl,
+        printf("[get_pte] %lx %d, index: %lu, pde: %p\n", va, lvl,
             get_lvl_index(va, lvl), pde);
         return &pde[get_lvl_index(va, lvl)];
     }
