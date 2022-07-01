bc3480676a2138e824e5a3fffd8358db88b6737a
Weixi Zhu <wxzhu@rice.edu>
2022-07-01 00:57:29 -0500

    add back thread but allows sleeping

--- mad_cdev/pm_cdev.c
+++ mad_cdev/pm_cdev.c
@@ -38,7 +38,7 @@ static inline void free_x97_page(vm_page_t m)
 }
 
 #define GB_PAGES 1024 * 1024 / 4
-size_t npages = GB_PAGES / 10;
+size_t npages = GB_PAGES / 100;
 
 int init_pm(struct gmem_mmu_ops *ops) {
     first_x97_page = vm_page_alloc_contig(NULL, 0, VM_ALLOC_NORMAL | VM_ALLOC_NOBUSY | VM_ALLOC_NOOBJ,
