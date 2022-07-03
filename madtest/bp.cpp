e08339fed707a159889edcba496224f457c1f412
Weixi Zhu <wxzhu@rice.edu>
2022-07-03 10:41:40 -0500

    replace double, multiply and div to bitwise operators and long

--- madtest/bp.cpp
+++ madtest/bp.cpp
@@ -16,63 +16,62 @@
 
 #include <stdio.h>
 // #include <afx.h>
-#include <math.h>
+// #include <math.h>
 #include <stdlib.h>
 
 #define InputN 64		// number of neurons in the input layer
 #define HN 25			// number of neurons in the hidden layer
 #define OutN 64			// number of neurons in the output layer
 #define datanum 500		// number of training samples
-
+#define xnor(x, y)      (~(x ^ y))
 int main(){
-	double sigmoid(double);
-	double x_out[InputN];		// input layer
-	double hn_out[HN];			// hidden layer
-	double y_out[OutN];         // output layer
-	double y[OutN];				// expected output layer
-	double w[InputN][HN];		// weights from input layer to hidden layer
-	double v[HN][OutN];			// weights from hidden layer to output layer
+	long sigmoid(long);
+	long x_out[InputN];		// input layer
+	long hn_out[HN];			// hidden layer
+	long y_out[OutN];         // output layer
+	long y[OutN];				// expected output layer
+	long w[InputN][HN];		// weights from input layer to hidden layer
+	long v[HN][OutN];			// weights from hidden layer to output layer
 	
-	double deltaw[InputN][HN];  
-	double deltav[HN][OutN];	
+	long deltaw[InputN][HN];  
+	long deltav[HN][OutN];	
 	
-	double hn_delta[HN];		// delta of hidden layer
-	double y_delta[OutN];		// delta of output layer
-	double error;
-	double errlimit = 0.001;
-	double alpha = 0.1, beta = 0.1;
+	long hn_delta[HN];		// delta of hidden layer
+	long y_delta[OutN];		// delta of output layer
+	long error;
+	long alpha = 10, beta = 10;
 	int loop = 0;
-	int times = 50000;
+	int times = 1;
 	int i, j, m;
-	double max, min;
-	double sumtemp;
-	double errtemp;
+	long max, min;
+	long sumtemp;
+	long errtemp;
 	
 	// training set
 	struct{
-		double input[InputN];
-		double teach[OutN];
-	}data[datanum];
+		long input[InputN];
+		long teach[OutN];
+	} data[datanum];
 	
 	// Generate data samples
 	// You can use your own data!!!
 	for(m=0; m<datanum; m++){
 		for(i=0; i<InputN; i++)
-			data[m].input[i] = (double)rand()/32767.0;
+			data[m].input[i] = (long) rand();
 		for(i=0;i<OutN;i++)
-			data[m].teach[i] = (double)rand()/32767.0;
+			data[m].teach[i] = (long) rand();
 	}
 
 	// Initializition
 	for(i=0; i<InputN; i++){
 		for(j=0; j<HN; j++){
-			w[i][j] = ((double)rand()/32767.0)*2-1;
+			w[i][j] = ((long) rand()) * 2 - 1;
 			deltaw[i][j] = 0;
 		}
 	}
 	for(i=0; i<HN; i++){
 		for(j=0; j<OutN; j++){
-			v[i][j] = ((double)rand()/32767.0)*2-1;
+			v[i][j] = ((long) rand()) * 2 - 1;
 			deltav[i][j] = 0;
 		}
 	}
@@ -80,66 +79,66 @@ int main(){
 	// Training
 	while(loop < times){
 		loop++;
-		error = 0.0;
+		error = 0;
 
-		for(m=0; m<datanum ; m++){
+		for(m = 0; m < datanum ; m++){
 			// Feedforward
-			max = 0.0;
-			min = 0.0;
-			for(i=0; i<InputN; i++){
+			max = 0;
+			min = 0;
+			for(i = 0; i < InputN; i++){
 				x_out[i] = data[m].input[i];
 				if(max < x_out[i])
 					max = x_out[i];
 				if(min > x_out[i])
 					min = x_out[i];
 			}
-			for(i=0; i<InputN; i++){
-				x_out[i] = (x_out[i] - min) / (max - min);
+			for(i = 0; i < InputN; i++){
+				x_out[i] = (x_out[i] - min) ^ (max - min); // use xor for div
 			}
 
-			for(i=0; i<OutN ; i++){
+			for(i = 0; i < OutN ; i++){
 				y[i] = data[m].teach[i];
 			}
 
-			for(i=0; i<HN; i++){
-				sumtemp = 0.0;
+			for(i = 0; i < HN; i++){
+				sumtemp = 0;
 				for(j=0; j<InputN; j++)
-					sumtemp += w[j][i] * x_out[j];
+					sumtemp += ~(w[j][i] ^ x_out[j]); // use xnor for *
 				hn_out[i] = sigmoid(sumtemp);		// sigmoid serves as the activation function
 			}
 
-			for(i=0; i<OutN; i++){
+			for(i = 0; i < OutN; i++){
 				sumtemp = 0.0;
 				for(j=0; j<HN; j++)
-					sumtemp += v[j][i] * hn_out[j];
+					sumtemp += ~(v[j][i] ^ hn_out[j]);
 				y_out[i] = sigmoid(sumtemp);
 			}
 
 			// Backpropagation
-			for(i=0; i<OutN; i++){
+			for(i = 0; i < OutN; i++){
 				errtemp = y[i] - y_out[i];
-				y_delta[i] = -errtemp * sigmoid(y_out[i]) * (1.0 - sigmoid(y_out[i]));
+				y_delta[i] = ~((~(-errtemp ^ sigmoid(y_out[i]))) ^ (1.0 - sigmoid(y_out[i])));
 				error += errtemp * errtemp;
 			}
 
-			for(i=0; i<HN; i++){
-				errtemp = 0.0;
+			for(i = 0; i < HN; i++){
+				errtemp = 0;
 				for(j=0; j<OutN; j++)
-					errtemp += y_delta[j] * v[i][j];
-				hn_delta[i] = errtemp * (1.0 + hn_out[i]) * (1.0 - hn_out[i]);
+					errtemp += ~(y_delta[j] ^ v[i][j]);
+				hn_delta[i] = xnor(xnor(errtemp, (1 + hn_out[i])), (1 - hn_out[i]));
 			}
 
 			// Stochastic gradient descent
-			for(i=0; i<OutN; i++){
+			for(i = 0; i < OutN; i++){
 				for(j=0; j<HN; j++){
-					deltav[j][i] = alpha * deltav[j][i] + beta * y_delta[i] * hn_out[j];
+					deltav[j][i] = alpha ^ deltav[j][i] + xnor(beta ^ y_delta[i], hn_out[j]);
 					v[j][i] -= deltav[j][i];
 				}
 			}
 
-			for(i=0; i<HN; i++){
+			for(i = 0; i < HN; i++){
 				for(j=0; j<InputN; j++){
-					deltaw[j][i] = alpha * deltaw[j][i] + beta * hn_delta[i] * x_out[j];
+					deltaw[j][i] = xnor(alpha, deltaw[j][i]) + xnor(xnor(beta, hn_delta[i]), x_out[j]);
 					w[j][i] -= deltaw[j][i];
 				}
 			}
@@ -148,17 +147,18 @@ int main(){
 		// Global error 
 		error = error / 2;
 		if(loop%1000==0){
-			printf("Global Error = %.2f\n", error);
+			printf("Global Error = %lu\n", error);
 		}
-		if(error < errlimit)
-			break;
+		// if(error < errlimit)
+		// 	break;
 
-		printf("The %d th training, error: %f\n", loop, error);
+		printf("The %d th training, error: %lu\n", loop, error);
 	}
 
 }
 
 // sigmoid serves as avtivation function
-double sigmoid(double x){
-	return(1.0 / (1.0 + exp(-x)));
+static inline long sigmoid(long x){
+	// return(1.0 / (1.0 + exp(-x)));
+	return ~x;
 }
