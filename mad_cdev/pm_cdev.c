cd6c2422ecbe4d107c0506c36caa6a97d0c3265e
Weixi Zhu <wxzhu@rice.edu>
2022-07-06 14:55:55 -0500

    use 100MB pages

--- mad_cdev/pm_cdev.c
+++ mad_cdev/pm_cdev.c
@@ -61,8 +61,8 @@ static inline void free_x97_page(vm_page_t m)
 		printf("The x97 page to free does not exist in activelist\n");
 }
 
-#define GB_PAGES 1024 * 1024 / 4
-size_t npages = GB_PAGES / 10; // 100MB pages
+#define MB_PAGES 1024 / 4
+size_t npages = MB_PAGES * 100; // 100MB pages
 
 int init_pm(struct gmem_mmu_ops *ops) {
     first_x97_page = vm_page_alloc_contig(NULL, 0, VM_ALLOC_NORMAL | VM_ALLOC_NOBUSY | VM_ALLOC_NOOBJ,
